8060fb829a0aa374eb0322d56d4875d733746f58
vsailer <77106748+vsailer@users.noreply.github.com>
2021-08-20 10:31:51 +0200

    render_to_response --> render

--- emencia/django/newsletter/admin/contact.py
+++ emencia/django/newsletter/admin/contact.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import reverse
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 from django.utils.translation import ugettext as _
 from django.http import HttpResponseRedirect
@@ -141,7 +141,7 @@ class ContactAdmin(admin.ModelAdmin):
                    'root_path': self.admin_site.root_path,
                    'app_label': opts.app_label}
 
-        return render_to_response(request, 'newsletter/contact_import.html',
+        return render(request, 'newsletter/contact_import.html',
                                   context)
 
     def filtered_request_queryset(self, request):
